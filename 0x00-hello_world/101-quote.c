0c46fd02f04b7cd9383b4f794eb85218cf586675
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-02-16 22:07:22 +0000

    Yeah

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 
 /**
 	 * main - Entry point
-	 * Description:  
+	 * Description
 	 * Return: Always 1 (Failure)
-	 */
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
