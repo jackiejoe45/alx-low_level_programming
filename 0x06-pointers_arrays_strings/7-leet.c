9d35e34bf59ae62dfcfd2e2cbcf4503d73b2f607
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-03 00:35:50 +0000

    Try 4

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 /**
   * leet - encodes a string into 1337O (leetcode)
-  * @str : char array
+  * @ch : char array
   * Return: char type
   */
-char *leet(char *str)
+char *leet(char *ch)
 {
-	int i, j;
-	char leet_map[128] = { 0 };
-	
-	leet_map['a'] = leet_map['A'] = '4';
-	leet_map['e'] = leet_map['E'] = '3';
-	leet_map['o'] = leet_map['O'] = '0';
-	leet_map['t'] = leet_map['T'] = '7';
-	leet_map['l'] = leet_map['L'] = '1';
-	
-	for (i = 0; str[i] != '\0'; i++)
+	int i=0, j;
+	char leet[9] = { 'O','L','?','E','A','?','?','T','\0' };
+
+	while (ch[i] != '\0')
 	{
-		if (leet_map[str[i]] != 0)
+		j = 0;
+
+		while (leet[j] != '\0')
 		{
-			str[i] = leet_map[str[i]];
+			if (ch[i] == leet[j] + 32 || ch[i] == leet [j])
+			{
+				ch[i]= j + '0';
+			}
+			j++;
 		}
+		i++;
 	}
-	return (str);
-}
+	return (ch);
