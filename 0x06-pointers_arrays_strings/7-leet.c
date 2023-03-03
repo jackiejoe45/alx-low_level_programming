ca0e59dc9b94561906b8b5312a6e61b3e863d860
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-03 00:12:10 +0000

    Try 3

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,21 @@
   */
 char *leet(char *str)
 {
-	char *ptr = str;
-	char leet_chars[] = "aAeEoOtTlL";
-	char leet_nums[] = "44337011";
 	int i, j;
-
-	while (*ptr)
+	char leet_map[128] = { 0 };
+	
+	leet_map['a'] = leet_map['A'] = '4';
+	leet_map['e'] = leet_map['E'] = '3';
+	leet_map['o'] = leet_map['O'] = '0';
+	leet_map['t'] = leet_map['T'] = '7';
+	leet_map['l'] = leet_map['L'] = '1';
+	
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i = 0;
-		while (leet_chars[i])
+		if (leet_map[str[i]] != 0)
 		{
-			if (*ptr == leet_chars[i])
-			{
-				*ptr = leet_nums[i];
-				break;
-			}
-			i++;
+			str[i] = leet_map[str[i]];
 		}
-		ptr++;
 	}
 	return (str);
 }
