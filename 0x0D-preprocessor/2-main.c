ab776ffcc90d634426f0e7cf59e22ca9bfead048
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-20 19:35:48 +0000

    Preprocessor Functions in C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+ * main - returns the file that was compiled
+ * Return - Always Success(0);
+ */
+int main(void)
+{
+	printf("done!")
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
+#include <math.h>
+#define ABS(x) (sqrt((x) * (x)))
+
+
+#endif /* 3-FUNCTION_LIKE_MACRO_H */
