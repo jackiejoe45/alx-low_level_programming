03a7774faa499eaa7fcbedb7af6bc1184e93bbe6
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-20 19:39:54 +0000

    Preprocessor Functions in C

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,4 +2,4 @@
 #define 0_OBJECT_LIKE_MACRO_H
 #define SIZE 1024
 
-#endif /* 0-OBJECT_LIKE_MACRO_H */
+#endif /* 0_OBJECT_LIKE_MACRO_H */
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,4 +4,4 @@
 #define ABS(x) (sqrt((x) * (x)))
 
 
-#endif /* 3-FUNCTION_LIKE_MACRO_H */
+#endif /* 3_FUNCTION_LIKE_MACRO_H */
