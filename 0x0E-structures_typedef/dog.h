585f95a3c19b1646dbcb6f8afcbe63e455440ff2
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-20 22:39:12 +0000

    Structs in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif /* DOG_H */
 /**
  * struct dog - a struct of a dogs basic details
@@ -17,3 +13,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
