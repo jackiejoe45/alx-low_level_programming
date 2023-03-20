17c6dd14afa6fad2c8ea2bd2fef3db1f02aaa94a
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-20 22:50:08 +0000

    Structs in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#endif /* DOG_H */
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +7,7 @@
  * struct dog - a struct of a dogs basic details
  * @name: name of dog(String)
  * @age: dog's age (integer)
- * @c: name of owner(String)
+ * @owner: name of owner(String)
  */
 typedef struct dog
 {
@@ -20,3 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif /* DOG_H*/
