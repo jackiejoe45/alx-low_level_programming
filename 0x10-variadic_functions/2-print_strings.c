d5a0ffe24dc31246753ebc4d507c77ef680c6025
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-03-24 16:09:28 +0000

    C variadic function files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,16 +15,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char *);
-
-                if (str == NULL)
+		if (str == NULL)
 		{
 			printf("(nil)");
 		}
-                else
+		else
 		{
 			printf("%s", str);
 		}
-                if (separator != NULL && i != n - 1)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
