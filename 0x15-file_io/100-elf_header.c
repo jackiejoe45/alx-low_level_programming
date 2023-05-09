0d37ef1687fbf60f7b76fbf067c9e4c5699af876
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-05-09 08:49:24 +0000

    File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,158 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <elf.h>
 
+
 /**
- * print_error - returns error mssge
- * @msg: type of error
+ * display_error - display error message on stderr
+ * @msg: error message to display
  */
-void print_error(char *msg)
+void display_error(char *msg)
 {
 	fprintf(stderr, "Error: %s\n", msg);
-	exit(98);
 }
 
 /**
- * print_elf_header - as it says
- * @filename: name of file
+ * read_elf_header - read ELF header from file descriptor into buffer
+ * @fd: file descriptor of ELF file
+ * @header: pointer to buffer to store ELF header
  */
-void print_elf_header(char *filename)
+void read_elf_header(int fd, Elf64_Ehdr *header)
 {
-	int fd, ret, i;
-	Elf64_Ehdr ehdr;
-	char magic[5];
+	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+	{
+		perror("Error reading ELF header");
+		exit(EXIT_FAILURE);
+	}
+	if (memcmp(header->e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Invalid ELF magic number\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
-	memset(&ehdr, 0, sizeof(ehdr));
+/**
+ * print_elf_header - print contents of ELF header to stdout
+ * @header: pointer to buffer containing ELF header
+ */
+void print_elf_header(Elf64_Ehdr *header)
+{
+	int i;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-	{
-		print_error("Could not open file");
-	}
-	ret = read(fd, &ehdr, sizeof(ehdr));
-	if (ret < 0)
-	{
-		print_error("Could not read ELF header");
-	}
-	memcpy(magic, ehdr.e_ident, 4);
-	magic[4] = '\0';
-	if (strcmp(magic, ELFMAG) != 0)
-	{
-		print_error("Not an ELF file");
-	}
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x ", ehdr.e_ident[i]);
+		printf("%02x ", header->e_ident[i]);
 	}
 	printf("\n");
-	if (ehdr.e_ident[EI_CLASS] == ELFCLASS32)
+	if (header->e_ident[EI_CLASS] == ELFCLASS64)
 	{
-		printf("  Class:                           ELF32\n");
-	}
-	else
+		printf("  Class:                             ELF64\n");
+	} else
 	{
-		printf("  Class:                           ELF64\n");
+		printf("  Class:                             ELF32\n");
 	}
-	close(fd);
+	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+	{
+		printf("  Data:                              2's complement, little endian\n");
+	} else
+	{
+		printf("  Data:                              2's complement, big endian\n");
+	}
+	printf("  Version:                           %d\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %d\n", header->e_type);
+	printf("  Machine:                           %d\n", header->e_machine);
+	printf("  Version:                           %d\n", header->e_version);
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+	printf("  Start of program headers:          %ld (bytes in file)\n", header->e_phoff);
+	printf("  Start of section headers:          %ld (bytes in file)\n", header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
 }
 
 /**
- * print_elf_header_cont - continued
- * @filename: name of file
+ * format_magic - format the ELF magic number as a string
+ * @buf: buffer to store formatted string
+ * @magic: pointer to buffer containing the ELF magic number
  */
-void print_elf_header_cont(char *filename)
+void format_magic(char *buf, unsigned char *magic)
 {
-	int fd;
-	Elf64_Ehdr ehdr;
-
-	memset(&ehdr, 0, sizeof(ehdr));
-	fd = open(filename, O_RDONLY);
-
-	if (ehdr.e_ident[EI_DATA] == ELFDATA2LSB)
-	{
-		printf("  Data:                            2's complement, little endian\n");
-	}
-	else
-	{
-		printf("  Data:                            2's complement, big endian\n");
-	}
-	printf("  Version:                        %d\n", ehdr.e_ident[EI_VERSION]);
-	printf("  OS/ABI:                          ");
-	switch (ehdr.e_ident[EI_OSABI])
-	{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_LINUX:
-			printf("UNIX - Linux\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
-	}
-	printf("  ABI Version:                  %d\n", ehdr.e_ident[EI_ABIVERSION]);
-	printf("  Type:                            ");
-	close(fd);
-}
-/**
- * print_elf_header_cont_2 - continued
- * @filename: name of file
- */
-void print_elf_header_cont_2(char *filename)
-{
-	int fd;
-	Elf64_Ehdr ehdr;
-
-	memset(&ehdr, 0, sizeof(ehdr));
-	fd = open(filename, O_RDONLY);
-
-	switch (ehdr.e_type)
-	{
-		case ET_NONE:
-			printf("NONE (Unknown type)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)\n");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("<unknown>\n");
-			break;
-	}
-	printf("  Entry point address:             %p\n", (void *)ehdr.e_entry);
-	close(fd);
+	sprintf(buf, "%02x %02x %02x %02x", magic[0], magic[1], magic[2], magic[3]);
 }
 
 /**
- * main - entry point
- * @argc: number of params
- * @argv: parameters
- * Return: 0(Success)
+ * main - entry point of the program
+ * @argc: number of command line arguments
+ * @argv: array of command line argument strings
+ * Return: 0 if successful, 98 if error occurred
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	char  *filename = argv[1];
+	int fd = open(filename, O_RDONLY);
+	Elf64_Ehdr elf_header;
 	if (argc != 2)
 	{
-		print_error("Usage: elf_header elf_filename");
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (98);
 	}
-	print_elf_header(argv[1]);
-	print_elf_header_cont(argv[1]);
-	print_elf_header_cont_2(argv[1]);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
+		return (98);
+	}
+	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+	{
+		fprintf(stderr, "Error: Cannot read ELF header from file '%s'\n", filename);
+		close(fd);
+		return (98);
+	}
+	print_elf_header(&elf_header);
+	close(fd);
 	return (0);
 }
