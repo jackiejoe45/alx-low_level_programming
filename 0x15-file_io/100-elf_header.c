ce5748069fa5289e6e0610b6b8fd8eb5e134cefe
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-05-25 18:00:19 +0000

    Elf header corrections

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,124 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <elf.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+#define BUF_SIZE 64
 
-/**
- * display_error - display error message on stderr
- * @msg: error message to display
- */
-void display_error(char *msg)
+void print_error(const char *message)
 {
-	fprintf(stderr, "Error: %s\n", msg);
+	fprintf(stderr, "%s\n", message);
+	exit(98);
 }
 
-/**
- * read_elf_header - read ELF header from file descriptor into buffer
- * @fd: file descriptor of ELF file
- * @header: pointer to buffer to store ELF header
- */
-void read_elf_header(int fd, Elf64_Ehdr *header)
+void print_elf_header(int fd)
 {
-	if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{
-		perror("Error reading ELF header");
-		exit(EXIT_FAILURE);
-	}
-	if (memcmp(header->e_ident, ELFMAG, SELFMAG) != 0)
-	{
-		fprintf(stderr, "Invalid ELF magic number\n");
-		exit(EXIT_FAILURE);
-	}
-}
-
-/**
- * print_elf_header - print contents of ELF header to stdout
- * @header: pointer to buffer containing ELF header
- */
-void print_elf_header(Elf64_Ehdr *header)
-{
+	unsigned char buf[BUF_SIZE];
 	int i;
+
+	if (read(fd, buf, sizeof(buf)) != sizeof(buf))
+		print_error("Failed to read ELF header");
+
+	if (memcmp(buf, "\x7f ELF", 4) != 0)
+		print_error("Not an ELF file");
 
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
+	for (i = 0; i < 16; i++)
+		printf("%02x ", buf[i]);
 	printf("\n");
-	if (header->e_ident[EI_CLASS] == ELFCLASS64)
+
+	printf("  Class:                             ");
+	switch (buf[4])
 	{
-		printf("  Class:                             ELF64\n");
-	} else
-	{
-		printf("  Class:                             ELF32\n");
+		case 1:
+			printf("ELF32\n");
+			break;
+		case 2:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("<unknown>\n");
 	}
-	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+
+	printf("  Data:                              ");
+	switch (buf[5])
 	{
-		printf("  Data:                              2's complement, little endian\n");
-	} else
-	{
-		printf("  Data:                              2's complement, big endian\n");
+		case 1:
+			printf("2's complement, little endian\n");
+			break;
+		case 2:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("<unknown>\n");
 	}
-	printf("  Version:                           %d\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", header->e_type);
-	printf("  Machine:                           %d\n", header->e_machine);
-	printf("  Version:                           %d\n", header->e_version);
-	printf("  Entry point address:               0x%lx\n", header->e_entry);
-	printf("  Start of program headers:          %ld (bytes in file)\n", header->e_phoff);
-	printf("  Start of section headers:          %ld (bytes in file)\n", header->e_shoff);
-	printf("  Flags:                             0x%x\n", header->e_flags);
-	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
-	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
-	printf("  Number of program headers:         %d\n", header->e_phnum);
-	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
-	printf("  Number of section headers:         %d\n", header->e_shnum);
-	printf("  Section header string table index: %d\n", header->e_shstrndx);
+
+	printf("  Version:                           %d (current)\n", buf[6]);
+
+	printf("  OS/ABI:                            ");
+	switch (buf[7])
+	{
+		case 0:
+			printf("UNIX - System V\n");
+			break;
+		case 1:
+			printf("HP-UX\n");
+			break;
+		case 2:
+			printf("NetBSD\n");
+			break;
+		case 3:
+			printf("Linux\n");
+			break;
+		case 6:
+			printf("Sun Solaris\n");
+			break;
+		case 9:
+			printf("FreeBSD\n");
+			break;
+		default:
+			printf("<unknown>\n");
+	}
+
+	printf("  ABI Version:                       %d\n", buf[8]);
+
+	printf("  Type:                              ");
+	switch (*(unsigned short *)(buf + 16))
+	{
+		case 1:
+			printf("REL (Relocatable file)\n");
+			break;
+		case 2:
+			printf("EXEC (Executable file)\n");
+			break;
+		case 3:
+			printf("DYN (Shared object file)\n");
+			break;
+		default:
+			printf("<unknown>\n");
+	}
+
+	printf("  Entry point address:               0x");
+	for (i = 0; i < 8; i++)
+		printf("%02x", buf[24 + i]);
+	printf("\n");
 }
 
-/**
- * format_magic - format the ELF magic number as a string
- * @buf: buffer to store formatted string
- * @magic: pointer to buffer containing the ELF magic number
- */
-void format_magic(char *buf, unsigned char *magic)
-{
-	sprintf(buf, "%02x %02x %02x %02x", magic[0], magic[1], magic[2], magic[3]);
-}
-
-/**
- * main - entry point of the program
- * @argc: number of command line arguments
- * @argv: array of command line argument strings
- * Return: 0 if successful, 98 if error occurred
- */
 int main(int argc, char *argv[])
 {
-	char  *filename = argv[1];
-	int fd = open(filename, O_RDONLY);
-	Elf64_Ehdr elf_header;
-	if (argc != 2)
+	const char *filename;
+	int fd;
+
+		if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-		return (98);
+		print_error("Usage: elf_header elf_filename");
 	}
+
+	filename = argv[1];
+
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
-		fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
-		return (98);
+		print_error("Failed to open file");
 	}
-	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-	{
-		fprintf(stderr, "Error: Cannot read ELF header from file '%s'\n", filename);
-		close(fd);
-		return (98);
-	}
-	print_elf_header(&elf_header);
+
+	print_elf_header(fd);
+
 	close(fd);
 	return (0);
 }
